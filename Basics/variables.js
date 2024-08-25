f6a733e862ba13e6c53d42d529ddb2934c96a515
Abhyuday-M <108abhaymishr@gmail.com>
2024-08-25 08:44:52 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const account_no = 1122;                     // Can't change this value of this variable
+let account_email = "ABC123@google.com";
+var account_password = "123";
+
+// Prefer not to use var 
+
+
+console.log(account_no);
+console.table([account_email, account_no, account_password]);
